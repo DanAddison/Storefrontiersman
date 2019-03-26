3e8c587ddeba959efefad8dc668870e7b8f61998
Dan Addison <danaddisoncreative@gmail.com>
2019-03-26 12:24:02 +0000

    Start tracking parent theme, gulp setup, ACF + Woo

---
+++ wp-content/themes/gulp-dev/Gulpfile.js
@@ -0,0 +1,64 @@
+var themename = 'storefrontiersman';
+
+var gulp = require('gulp'),
+	// Prepare and optimize code etc
+	autoprefixer = require('autoprefixer'),
+	browserSync = require('browser-sync').create(),
+	jshint = require('gulp-jshint'),
+	postcss = require('gulp-postcss'),
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
+
+	// Only work with new or updated files
+	newer = require('gulp-newer'),
+
+	// Name of working theme folder
+	root = '../' + themename + '/',
+	scss = root + 'assets/' + 'sass/',
+	js = root + 'assets/' + 'js/',
+	img = root + 'assets/' + 'images/',
+	languages = root + 'languages/';
+
+
+// CSS via Sass and Autoprefixer
+gulp.task('css', function() {
+	return gulp.src(scss + 'style.scss')
+	.pipe(sourcemaps.init())
+	.pipe(sass({
+		outputStyle: 'expanded', 
+		indentType: 'tab',
+		indentWidth: '1'
+	}).on('error', sass.logError))
+	.pipe(postcss([
+		autoprefixer('last 2 versions', '> 1%')
+	]))
+	.pipe(sourcemaps.write(scss + 'maps'))
+	.pipe(gulp.dest(root));
+});
+
+
+// JavaScript
+gulp.task('javascript', function() {
+	return gulp.src([js + '*.js'])
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'))
+	.pipe(gulp.dest(js));
+});
+
+
+// Watch everything
+gulp.task('watch', function() {
+	browserSync.init({ 
+		open: 'external',
+		proxy: 'storefrontiersman.localhost',
+		port: 8080
+	});
+	gulp.watch([root + '**/*.css', root + '**/*.scss' ], ['css']);
+	gulp.watch(js + '**/*.js', ['javascript']);
+	gulp.watch(root + '**/*').on('change', browserSync.reload);
+});
+
+// '**/' means 'zero or more directories'
+
+// Default task (runs at initiation: gulp --verbose)
+gulp.task('default', ['watch']);
