0b802002b690362a88b72b2d466554d135b6619d
Dan Addison <danaddisoncreative@gmail.com>
2019-03-27 10:56:33 +0000

    Remove sections from the customizer

--- wp-content/themes/storefrontiersman/functions.php
+++ wp-content/themes/storefrontiersman/functions.php
@@ -35,6 +35,7 @@ function da_font_awesome_enqueue() {
 	wp_enqueue_style( 'font-awesome-5-all', '//use.fontawesome.com/releases/v5.3.1/css/all.css' );
 }
 
+// fancybox
 add_action( 'wp_enqueue_scripts', 'da_lightbox' );
 function da_lightbox() {
 	if ( is_page_template( 'template-project.php' ) ) {
@@ -91,26 +92,15 @@ function da_widgets_init() {
 }
 add_action( 'widgets_init', 'da_widgets_init' );
 	
-// create instagram feed widget:	
-function da_instagram_widget() {
-	// first, if this sidebar is not active then return (do not execute the rest of the function):
-	if( ! is_active_sidebar( 'above_footer_widget' ) )
-		return;
-	// now, display this sidebar if we're on the following pages, or on any type of events page (uses a tidy function from the Tribe Events Calendar plugin to check for all of these):
-	if ( is_page ( array( 'bio', 'contact', 'publications' ) ) || tribe_is_event_query() ) {
-		dynamic_sidebar( 'above_footer_widget' );
-	}
-}
-add_action( 'storefront_before_footer', 'da_instagram_widget', 5 );
  
-// remove 'sort by average rating' from the dropdown on a product page (reinstate if there are eventually lots of ratings):
-// add_filter ( 'woocommerce_catalog_orderby', 'da_catalog_orderby', 20);
-// function da_catalog_orderby( $orderby ){
-// unset ($orderby['rating']);
-// unset ($orderby['popularity']);
-// $orderby['date'] = __('Sort by date: newest to oldest', 'woocommerce');
-// return $orderby;
-// }
+// remove 'sort by average rating' from the dropdown on shop pages
+add_filter ( 'woocommerce_catalog_orderby', 'da_catalog_orderby', 20);
+function da_catalog_orderby( $orderby ){
+unset ($orderby['rating']);
+unset ($orderby['popularity']);
+$orderby['date'] = __('Sort by date: newest to oldest', 'woocommerce');
+return $orderby;
+}
 
    
 // remove stuff:
@@ -123,10 +113,7 @@ function da_remove_storefront_actions() {
 	// remove secondary menu that usually lives in the header:
 	remove_action( 'storefront_header', 'storefront_secondary_navigation', 30 );
 
-	// remove page header when using hero if I want page title inside hero section:
-	// remove_action( 'storefront_page', 'storefront_page_header', 10 );
-
-	// remove sorting dropdown and result count on shop pages entirely (looks ugly, probably not that useful?):
+	// remove some sorting dropdowns
 	// remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 10 );
 	remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
 	remove_action( 'woocommerce_after_shop_loop', 'woocommerce_catalog_ordering', 10 );
@@ -146,46 +133,34 @@ function wheelbarrow_remove_breadcrumbs() {
     }
 }
 
-
-
 // remove the sidebars on all pages and archives, except those stated:
-add_action( 'wp', 'da_remove_sidebar_shop_page' );
-function da_remove_sidebar_shop_page() {
-	if ( ! is_page ( array( 'bio', 'contact' ) )) {		
-		remove_action( 'storefront_sidebar', 'storefront_get_sidebar', 10 );
-		add_filter( 'body_class', 'da_remove_sidebar_class_body', 10 );
-	}
-}
-
-function da_remove_sidebar_class_body( $wp_classes ) {
-	$wp_classes[] = 'page-template-template-fullwidth-php';
-	return $wp_classes;
-}
-
-add_action( 'woocommerce_before_shop_loop', 'da_archive_categories_list', 7 );
-
-// Add sections to shop page:
-// add_action( 'woocommerce_before_main_content', 'da_shop_page_sections' );
-// function da_shop_page_sections() {	
-// 	if ( is_shop() ) {	
-// 		add_action( 'woocommerce_before_shop_loop', 'storefront_featured_products', 8 );
-// 		// add_action( 'woocommerce_before_shop_loop', 'storefront_product_categories', 8 );
+// add_action( 'wp', 'da_remove_sidebar_shop_page' );
+// function da_remove_sidebar_shop_page() {
+// 	if ( ! is_page ( array( 'bio', 'contact' ) )) {		
+// 		remove_action( 'storefront_sidebar', 'storefront_get_sidebar', 10 );
+// 		add_filter( 'body_class', 'da_remove_sidebar_class_body', 10 );
 // 	}
 // }
+// 
+// function da_remove_sidebar_class_body( $wp_classes ) {
+// 	$wp_classes[] = 'page-template-template-fullwidth-php';
+// 	return $wp_classes;
+// }
 
+		
 // change wording of featured products section from 'we recommend':
 // add_filter( 'storefront_featured_products_args', 'da_featured_products' );
 // function da_featured_products( $args ){
-// 	$args = array(
-// 		'limit' => 3,
-// 		'columns' => 3,
-// 		'orderby' => 'name',
-// 		'title'	=> __( 'Featured Products', 'storefront' ),
-// 	);
-// 	return $args;
-// }
-
-// add product categories horizontal list on shop archive pages
+	// 	$args = array(
+		// 		'limit' => 3,
+		// 		'columns' => 3,
+		// 		'orderby' => 'name',
+		// 		'title'	=> __( 'Featured Products', 'storefront' ),
+		// 	);
+		// 	return $args;
+		// }
+				
+// add horizontal list of product categories on shop archives
 function da_archive_categories_list() {
 	$args = array(
 		'separator' => ' &sol;&sol; ',
@@ -193,28 +168,17 @@ function da_archive_categories_list() {
 		'taxonomy' => 'product_cat',
 		'title_li' => '',
 	);
-
+	
 	echo '<div class="menu--categories">
 	<p>Categories: </p>
 	<ul >';
 	wp_list_categories( $args );
 	echo '</ul></div>';
 }
+add_action( 'woocommerce_before_shop_loop', 'da_archive_categories_list', 7 );
 
 // remove add to cart button on product archives
 remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
-
-// changing number of columns for product category rows, if a 'Shop by Category' section is to be included on main shop page:
-// add_filter( 'storefront_product_categories_args', 'da_product_categories' );
-// function da_product_categories( $args ){
-// 	$args = array(
-// 		'limit' => 5,
-// 		'columns' => 5,
-// 		'orderby' => 'name',
-// 		'title'	=> __( 'Shop by Category', 'storefront' ),
-// 	);
-// 	return $args;
-// }
 
 // remove phone field in checkout:
 add_filter( 'woocommerce_checkout_fields', 'da_checkout_fields', 20 );
@@ -239,18 +203,6 @@ function da_hear_about_us ( $fields ){
 		);
 	return $fields;
 }
-
-/*
-// change cart to basket:
-// note: this does not also update any 'view cart' buttons, and if you change cart page name and slug to basket then cart cannot be found at all...
-add_filter( 'woocommerce_product_add_to_cart_text', 'da_add_to_cart_text', 10, 2 );
-
-add_filter( 'woocommerce_product_single_add_to_cart_text', 'da_add_to_cart_text', 10, 2 ); 
-function da_add_to_cart_text( $text, $product ) {
-	$text = __( 'Add to basket', 'woocommerce' );
-	return $text;
-}
-*/
 
 // add home icon to handheld footer bar:
 add_filter( 'storefront_handheld_footer_bar_links', 'da_add_home_link' );
@@ -313,24 +265,54 @@ function da_sub_footer() {
 	<?php
 }
 
-// Deregister core Gutenberg blocks
+/**
+ * Remove customizer sections (commented-out bits allow hiding from non-admin users only)
+ */
+function da_remove_css_section( $wp_customize ) {
+	// $user = wp_get_current_user();
+
+	// if ( ! $user->has_cap( 'manage_options' ) ) {
+		$wp_customize->remove_section( 'storefront_typography' );
+		$wp_customize->remove_section( 'storefront_footer' );
+		$wp_customize->remove_section( 'storefront_buttons' );
+		$wp_customize->remove_section( 'storefront_layout' );
+		$wp_customize->remove_section( 'storefront_more' );
+		$wp_customize->remove_section( 'header_image' );
+		$wp_customize->remove_section( 'background_image' );
+		$wp_customize->remove_section( 'static_front_page' );
+		$wp_customize->remove_section( 'custom_css' );
+		$wp_customize->remove_panel( 'widgets' );
+	// }
+}
+add_action( 'customize_register', 'da_remove_css_section', 15 );
+
+// Allow only some core Gutenberg blocks
 function wheelbarrow_allowed_block_types() {
 
 	return array(
 		'core/heading',
 		'core/paragraph',
 		'core/list',
+		'core/table',
+		'core/file',
+		'core/verse',
 		'core/quote',
 		'core/image',
-		'core/inline-image',
-		'core/spacer',
+		'core/cover',
+		'core/media-text',
 		'core/separator',
-		'core/file',
 		'core/shortcode',
-		'core-embed/facebook',
 		'core-embed/twitter',
 		'core-embed/instagram',
-		);
+		'core-embed/youtube',
+		'core-embed/vimeo',
+
+		// allow some blocks from WooCommerce Blocks plugin
+		'woocommerce/handpicked-products',
+		'woocommerce/featured-product',
+		'woocommerce/product-category',
+		'woocommerce/products-by-attribute',
+	);
 }
 
 add_filter( 'allowed_block_types', 'wheelbarrow_allowed_block_types', 10, 2 );
